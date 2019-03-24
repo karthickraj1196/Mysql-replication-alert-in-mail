c47ea9501befdec1642220f6dbea20f5af48b2bc
karthickraj1196 <karthickraj1196@gmail.com>
2019-03-24 15:38:21 +0530

    Update replication.sh

--- replication.sh
+++ replication.sh
@@ -3,10 +3,10 @@ author: Pushparajkarthick D(NPI) written at 24-03-2019
 '
 set -x;
 ###########Sysconfig################
-mysql -u "mysql_user" -h "mysql_host" -p"mysql_password" -P "mysql_port"  -e "show slave status\G" > /opt/SCRIPTS/3401.txt 
+mysql -u "mysql_user" -h "mysql_host" -p"mysql_password" -P "mysql_port"  -e "show slave status\G" > file.txt 
 
-IO_status=$(grep -E "Slave_IO_Running:" /opt/SCRIPTS/3401.txt | cut -d: -f2)
-SQL_status=$(grep -E "Slave_SQL_Running:" /opt/SCRIPTS/3401.txt | cut -d: -f2)
+IO_status=$(grep -E "Slave_IO_Running:" file.txt | cut -d: -f2)
+SQL_status=$(grep -E "Slave_SQL_Running:" file.txt | cut -d: -f2)
 
 if [ $IO_status == No ] || [ $SQL_status == No ];then
 
